da3574c9d910a13324df2e0a8f8a632e137ee322
DyatkoM <dyatkomaria9@gmail.com>
2023-12-07 04:09:40 +0300

    Created URL address for user.urls and employer_app.urls

--- job_search/job_search/urls.py
+++ job_search/job_search/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('user/', include('users.urls'), name='user'),
+    path('', include('employer_app.urls'), name='employer_app'),
 ]
