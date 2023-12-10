42b1a84a2e5ccf07d994d18381df34894de2ab85
DyatkoM <dyatkomaria9@gmail.com>
2023-12-11 00:20:12 +0300

    Added get_context_data for ShowVacancy and search_area

--- job_search/vacancy/views.py
+++ job_search/vacancy/views.py
@@ -15,3 +15,10 @@ class ShowVacancy(ListView, LoginRequiredMixin):
     context_object_name = 'all_vacancy'
     template_name = 'vacancy/list_vacancy.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        search_input = self.request.GET.get('search-area') or ''
+        if search_input:
+            context['all_vacancy'] = context['all_vacancy'].filter(title__startswith=search_input)
+            context['search_input'] = search_input
+        return context
