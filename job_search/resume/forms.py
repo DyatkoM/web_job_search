28b2005adaa40e516ec1d3b5d4c7d9d161255c5c
DyatkoM <dyatkomaria9@gmail.com>
2023-12-16 22:55:25 +0300

    The form line with the name "conditions" has been deleted and unnecessary fields have been removed

--- job_search/resume/forms.py
+++ job_search/resume/forms.py
@@ -9,8 +9,7 @@ class CreateForm(forms.ModelForm):
         'class': 'form_salary', 'placeholder': 'Заработная плата:'}))
     work_experience = forms.CharField(widget=forms.TextInput(attrs={
         'class': 'form_work_experience', 'placeholder': 'Опыт работы:'}))
-    conditions = forms.CharField(widget=forms.TextInput(attrs={
-        'class': 'form_conditions', 'placeholder': 'Условия:'}))
+
     contacts = forms.CharField(widget=forms.TextInput(attrs={
         'class': 'form_contacts', 'placeholder': 'Контакты и адрес:'}))
     skills = forms.CharField(widget=forms.TextInput(attrs={
@@ -20,6 +19,5 @@ class CreateForm(forms.ModelForm):
         model = Resume
         fields = [
             'title', 'salary', 'work_experience',
-            'duties', 'requirements', 'conditions',
             'contacts', 'skills'
         ]
